8f37705cdc6d3b74cdfd961c30fab75dc2c737f9
caio-gdp <git@sindest.com>
2023-02-03 18:30:14 -0300

    Migration DB

---
+++ Back/src/CadastroDigital/CadastroDigital.DataLayer/Migrations/20230203211834_Initial.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroDigital.DataLayer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PessoaFisica",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PessoaFisica", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PessoaJuridica",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cnpj = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PessoaJuridica", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoPessoa = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PessoaFisicaId = table.Column<int>(type: "int", nullable: true),
+                    PessoaJuridicaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pessoa_PessoaFisica_PessoaFisicaId",
+                        column: x => x.PessoaFisicaId,
+                        principalTable: "PessoaFisica",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pessoa_PessoaJuridica_PessoaJuridicaId",
+                        column: x => x.PessoaJuridicaId,
+                        principalTable: "PessoaJuridica",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoa_PessoaFisicaId",
+                table: "Pessoa",
+                column: "PessoaFisicaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoa_PessoaJuridicaId",
+                table: "Pessoa",
+                column: "PessoaJuridicaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+
+            migrationBuilder.DropTable(
+                name: "PessoaFisica");
+
+            migrationBuilder.DropTable(
+                name: "PessoaJuridica");
+        }
+    }
+}
