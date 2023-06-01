12dc2c8ea67ef1c9c901fdc8f8db3440dd0ffa99
caio-gdp <git@sindest.com>
2023-05-31 21:43:16 -0300

    ajustes menu e pre cadastro

--- Front/CadastroDigital-App/src/app/components/user/menu/menu.component.ts
+++ Front/CadastroDigital-App/src/app/components/user/menu/menu.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  passo : string = "precadastro";
+
   constructor() { }
 
   ngOnInit() {
+
+    if (this.passo != undefined){
+       (<HTMLInputElement>document.getElementById("a-" + this.passo)).style.borderLeft = "3px solid #fd0404";
+       (<HTMLInputElement>document.getElementById("a-" + this.passo)).style.backgroundColor = "#ebeaea";
+       (<HTMLInputElement>document.getElementById("i-" + this.passo)).style.color = "#fd0404";
+    }
   }
 
 }
